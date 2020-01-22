22bc6f6a5652eb7ecbb32d2782137c8fb7bf906a
iqbal <iqbalshorfana@gmail.com>
2020-01-22 17:30:02 +0700

    Bismillah Layanan Cloud

---
+++ application/logs/log-2019-10-25.php
@@ -0,0 +1,20 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-10-25 01:35:17 --> Severity: 8192 --> strpos(): Non-string needles will be interpreted as strings in the future. Use an explicit chr() call to preserve the current behavior C:\xampp\htdocs\kostlab\application\third_party\MX\Router.php 239
+ERROR - 2019-10-25 01:35:17 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'kostlabkit' C:\xampp\htdocs\kostlab\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-10-25 01:35:17 --> Unable to connect to the database
+ERROR - 2019-10-25 01:36:35 --> Severity: 8192 --> strpos(): Non-string needles will be interpreted as strings in the future. Use an explicit chr() call to preserve the current behavior C:\xampp\htdocs\kostlab\application\third_party\MX\Router.php 239
+ERROR - 2019-10-25 01:36:35 --> Severity: 8192 --> The each() function is deprecated. This message will be suppressed on further calls C:\xampp\htdocs\kostlab\application\third_party\MX\Modules.php 83
+ERROR - 2019-10-25 01:36:35 --> 404 Page Not Found: /index
+ERROR - 2019-10-25 01:36:40 --> Severity: 8192 --> strpos(): Non-string needles will be interpreted as strings in the future. Use an explicit chr() call to preserve the current behavior C:\xampp\htdocs\kostlab\application\third_party\MX\Router.php 239
+ERROR - 2019-10-25 01:36:40 --> Severity: 8192 --> The each() function is deprecated. This message will be suppressed on further calls C:\xampp\htdocs\kostlab\application\third_party\MX\Modules.php 83
+ERROR - 2019-10-25 01:36:40 --> 404 Page Not Found: /index
+ERROR - 2019-10-25 01:36:53 --> Severity: 8192 --> strpos(): Non-string needles will be interpreted as strings in the future. Use an explicit chr() call to preserve the current behavior C:\xampp\htdocs\kostlab\application\third_party\MX\Router.php 239
+ERROR - 2019-10-25 01:36:53 --> Severity: 8192 --> The each() function is deprecated. This message will be suppressed on further calls C:\xampp\htdocs\kostlab\application\third_party\MX\Modules.php 83
+ERROR - 2019-10-25 01:36:53 --> 404 Page Not Found: /index
+ERROR - 2019-10-25 05:50:13 --> Severity: Warning --> mysqli::real_connect(): (HY000/1049): Unknown database 'belajar_laravel' C:\xampp\htdocs\kostlab\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-10-25 05:50:13 --> Unable to connect to the database
+ERROR - 2019-10-25 05:51:06 --> 404 Page Not Found: /index
+ERROR - 2019-10-25 05:51:08 --> 404 Page Not Found: /index
+ERROR - 2019-10-25 05:52:23 --> 404 Page Not Found: /index
+ERROR - 2019-10-25 05:52:23 --> 404 Page Not Found: /index
---
+++ application/logs/log-2020-01-22.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-01-22 07:20:21 --> 404 Page Not Found: /index
+ERROR - 2020-01-22 07:20:27 --> 404 Page Not Found: /index
+ERROR - 2020-01-22 07:24:05 --> 404 Page Not Found: /index
+ERROR - 2020-01-22 07:24:08 --> 404 Page Not Found: /index
+ERROR - 2020-01-22 07:24:08 --> 404 Page Not Found: /index
+ERROR - 2020-01-22 07:26:26 --> 404 Page Not Found: /index
+ERROR - 2020-01-22 07:26:26 --> 404 Page Not Found: /index
